32c80784b73f5ef8353ecafb54943200e2c16cd2
Tarun Gunampalli <tarun.gunampalli@gmail.com>
2022-04-20 16:57:41 -0500

    fixed animation issue

--- skinning/Gui.js
+++ skinning/Gui.js
@@ -289,11 +289,11 @@ export class GUI {
     }
     setSkeleton(index, t) {
         const frame = Math.floor(t);
-        t -= frame;
+        const interp = t - frame;
         const bone = this.bones[index];
         const initialB = Vec3.difference(bone.initialEndpoint, bone.initialPosition);
         const l = initialB.length();
-        bone.rotation = Quat.slerp(this.keyFrames[frame][index], this.keyFrames[frame + 1][index], t);
+        bone.rotation = Quat.slerp(this.keyFrames[frame][index], this.keyFrames[frame + 1][index], interp);
         bone.endpoint = Vec3.sum(bone.position, initialB.multiplyByQuat(bone.rotation).normalize().scale(l));
         bone.children.forEach((c) => {
             const child = this.bones[c];
