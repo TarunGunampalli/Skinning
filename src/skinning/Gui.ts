218301f34f7c5e82c8459e5b924cda73be2c7573
Tarun Gunampalli <tarun.gunampalli@gmail.com>
2022-04-24 17:07:36 -0500

    playback starts at scrubber

--- src/skinning/Gui.ts
+++ src/skinning/Gui.ts
@@ -632,7 +632,7 @@ export class GUI implements IGUI {
 			case "KeyP": {
 				if (this.mode === Mode.edit && this.getNumKeyFrames() > 1) {
 					this.mode = Mode.playback;
-					this.time = 0;
+					this.time = this.scrubberTime == 1 ? 0 : this.scrubberTime * this.getMaxTime();
 					this.selectedKeyFrame = -1;
 					this.hoveredTick = -1;
 					this.animation.cylinder.setDraw(false);
